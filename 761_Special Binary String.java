18ab9a7548385a07bfd7ac698df437a6aa4ac508
auronoda <auronoda@hotmail.com>
2018-05-02 19:25:50 -0400

    Create 761_Special Binary String.java

---
+++ 761_Special Binary String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeLargestSpecial(String S) {
+        int i = 0; 
+        int count = 0;
+        List<String> res = new ArrayList<>();
+        for (int j = 0; j < S.length(); ++j) {
+            if (S.charAt(j) == '1') {
+                count++;
+            }
+            else {
+                count--;
+                if (count == 0) {
+                    res.add("1" + makeLargestSpecial(S.substring(i + 1, j)) + "0");
+                    i = j + 1;
+                }
+            }
+        }
+        Collections.sort(res, Collections.reverseOrder());
+        return String.join("",res);
+    }
+}
