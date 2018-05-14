4d71fe478362d60fc5d065776cdc33e639fa9f48
auronoda <auronoda@hotmail.com>
2018-05-13 23:20:47 -0400

    Create 461_Hamming Distance.java

---
+++ 461_Hamming Distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int res = 0;
+        while (x != y) {
+            if ((x & 1) != (y & 1)) {
+                res++;
+            }
+            x >>>= 1;
+            y >>>= 1;
+        }
+        return res;
+    }
+}
