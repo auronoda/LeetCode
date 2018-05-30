ad5397eb8e895b7bc49a5bf6b329d5a993b2e880
auronoda <auronoda@hotmail.com>
2018-05-29 21:26:59 -0400

    Create 837_New 21 Game.java

---
+++ 837_New 21 Game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double new21Game(int N, int K, int W) {
+        if (K == 0 || N >= K + W) {
+            return 1.0;
+        }
+        double[] dp = new double[N + 1];
+        dp[0] = 1;
+        double sum = 1;
+        double res = 0;
+        for (int i = 1; i <= N; ++i) {
+            dp[i] = sum / W;
+            if (i < K) {
+                sum += dp[i];
+            }
+            else {
+                res += dp[i];
+            }
+            
+            if (i >= W) {
+                sum -= dp[i - W];
+            }
+        }
+        return res;
+    }
+}
