113486390a4303acb5ee0423e8aa672febb81d3d
auronoda <auronoda@hotmail.com>
2018-06-21 21:48:07 -0400

    Create 538_Convert BST to Greater Tree.java

---
+++ 538_Convert BST to Greater Tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        int sum = 0; 
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            int val = cur.val;
+            cur.val += sum;
+            sum += val;
+            cur = cur.left;
+        }
+        return root;
+    }
+}
