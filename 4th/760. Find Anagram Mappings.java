f8fef0786533018de8597f4bb93c51bf6b652138
auronoda <auronoda@hotmail.com>
2018-07-04 16:00:42 -0400

    Create 760. Find Anagram Mappings.java

---
+++ 4th/760. Find Anagram Mappings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; ++i) {
+            map.put(B[i], i);
+        }
+        
+        int[] res = new int[A.length];
+        for (int i = 0; i < A.length; ++i) {
+            res[i] = map.get(A[i]);
+        }
+        return res;
+    }
+}
