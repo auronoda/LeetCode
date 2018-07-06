9b2351acbb933b76a402ee110e5d4158771e8a7b
auronoda <auronoda@hotmail.com>
2018-07-06 19:59:16 -0400

    Create 672. Bulb Switcher II.java

---
+++ 4th/672. Bulb Switcher II.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int flipLights(int n, int m) {
+        if (m == 0) return 1;
+        if (m == 1) return n == 1 ? 2 : n == 2 ? 3 : 4;
+        if (m == 2) return n == 1 ? 2 : n == 2 ? 4 : 7;
+        return n == 1 ? 2 : n == 2 ? 4 : 8;
+    }
+}
