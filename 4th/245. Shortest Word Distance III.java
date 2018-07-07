1c810f6159798c74c4be251586b216562128a630
auronoda <auronoda@hotmail.com>
2018-07-06 20:27:24 -0400

    Create 245. Shortest Word Distance III.java

---
+++ 4th/245. Shortest Word Distance III.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        List<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
+        for (int i = 0; i < words.length; ++i) {
+            if (words[i].equals(word1)) {
+                list1.add(i);
+            }
+            if (words[i].equals(word2)) {
+                list2.add(i);
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        int i = 0;
+        for (int idx1: list1) {
+            while (i < list2.size() && list2.get(i) < idx1) {
+                res = Math.min(res, idx1 - list2.get(i));
+                i++;
+            }
+            if (i < list2.size() && list2.get(i) == idx1) {
+                i++;
+            }
+            if (i < list2.size()) {
+                res = Math.min(res, list2.get(i) - idx1);
+            }
+        }
+        return res;
+    }
+}
